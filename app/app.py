e0d5f19e7fc98fb8f3b174e84a207f9e09f0d9de
kofi-teddy <agudogot@gmail.com>
2023-02-18 07:27:04 +0000

    database setup correctly

--- app/app.py
+++ app/app.py
@@ -17,12 +17,15 @@ async def hello():
         "message": "Hello hero!"
     }
 
+
 @app.on_event("startup")
 async def app_init():
     """
     initialize crutial application services
     """
-    db_client = AsyncIOMotorClient(settings.MANGO_CONNECTION_STRING)
+    # db_client = AsyncIOMotorClient(settings.MANGO_CONNECTION_STRING)
+    client = AsyncIOMotorClient(settings.MANGO_CONNECTION_STRING)
+    db_client = client["Cluster0"]
 
     await init_beanie(
         database=db_client,
