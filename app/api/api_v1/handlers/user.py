e321089a584185df76299ea27aa2b96df9a1328a
kofi-teddy <agudogot@gmail.com>
2023-02-18 08:50:46 +0000

    how to user lambda function

--- app/api/api_v1/handlers/user.py
+++ app/api/api_v1/handlers/user.py
@@ -1,12 +1,17 @@
-from fastapi import APIRouter
+import pymongo
+from fastapi import APIRouter, HTTPException, status
 
 from app.schemas.user_schema import UserAuth
+from app.services.user_service import UserService
 
 user_router = APIRouter()
 
 @user_router.post('/create', summary="create new user")
 async def create_user(data: UserAuth):
-    # return {
-    #     "message": "user router working"
-    # }
-    pass
+    try:
+        await UserService.create_user(data)
+    except pymongo.errors.DuplicateKeyError:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            default="User with this email or username already exist"
+        )
--- app/models/user_model.py
+++ app/models/user_model.py
@@ -1,18 +1,18 @@
 from datetime import datetime
 from uuid import UUID, uuid4
-
+from typing import Optional
 from beanie import Document, Indexed
 from pydantic import EmailStr, Field
 
 
 class User(Document):
-    user_id: UUID = Field(defualt_factory=uuid4)
+    user_id: str = Field(default_factory=lambda: uuid4().hex)
     username: str = Indexed(str, unique=True)
     email: Indexed(EmailStr, unique=True)
     hashed_password: str
-    first_name: str
-    last_name: str
-    disabled: bool
+    first_name: Optional[str]
+    last_name: Optional[str]
+    disabled: Optional[bool]
 
     def __repr__(self) -> str:
         return f"User {self.email}"
