6d05d2ec32684ae4afc51cd48d96c63b3c39b5c6
kofi-teddy <agudogot@gmail.com>
2023-02-19 07:42:14 +0000

    user out done

--- app/schemas/user_schema.py
+++ app/schemas/user_schema.py
@@ -1,7 +1,15 @@
 from pydantic import BaseModel, EmailStr, Field 
-
-
+from typing import Optional
+from uuid import UUID
 class UserAuth(BaseModel):
     email: EmailStr = Field(..., description="user email")
     username: str = Field(..., min_length=5, max_length=50, description="user username")
     password: str = Field(..., min_length=5, max_length=24, description="user password")
+
+
+class UserOut(BaseModel):
+    user_id: str
+    username: str
+    first_name: Optional[str]
+    last_name: Optional[str]
+    disabled: bool = False
